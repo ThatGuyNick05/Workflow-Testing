7d3402d6c1912629c0f79854fa8569082f31d2a1
ThatGuyNick <nickrudnick800@gmail.com>
2025-06-21 06:58:09 -0400

    Remove compiled JS files and ignore them

--- tests/index.test.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const index_1 = require('../src/index');
-test('adds 2 + 2 to equal 4', () => {
-  expect((0, index_1.add)(2, 2)).toBe(4);
-});
